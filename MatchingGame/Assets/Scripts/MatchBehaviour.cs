181fd9989ec3c1039db3b6c25e56eed9cd937dff
Dmitry Panchuk <10994198@uvu.edu>
2024-09-09 23:45:18 -0600

    Matching Game WIP

---
+++ MatchingGame/Assets/Scripts/MatchBehaviour.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MatchBehaviour : MonoBehaviour
+{
+
+    public ID idObj;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var otherID = other.GetComponent<IDContainerBehaviour>().idObj;
+        if (otherID == idObj)
+        {
+            Debug.Log("MATCH");
+        }
+    }
+}
