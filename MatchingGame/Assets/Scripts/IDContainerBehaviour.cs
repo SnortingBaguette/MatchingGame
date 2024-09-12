49391ea574e794a079a76ee28e366d6b608a0bde
Dmitry Panchuk <10994198@uvu.edu>
2024-09-12 01:01:53 -0600

    Completed Lab 1

--- MatchingGame/Assets/Scripts/IDContainerBehaviour.cs
+++ MatchingGame/Assets/Scripts/IDContainerBehaviour.cs
@@ -1,8 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class IDContainerBehaviour : MonoBehaviour
 {
     public ID idObj;
+
+    public UnityEvent startEvent;
+
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
 }
--- MatchingGame/Assets/Scripts/MatchBehaviour.cs
+++ MatchingGame/Assets/Scripts/MatchBehaviour.cs
@@ -1,16 +1,33 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MatchBehaviour : MonoBehaviour
 {
 
     public ID idObj;
+    public UnityEvent matchEvent, noMatchEvent, noMatchDelayedEvent;
 
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator OnTriggerEnter(Collider other)
     {
-        var otherID = other.GetComponent<IDContainerBehaviour>().idObj;
+
+        var tempObj = other.GetComponent<IDContainerBehaviour>();
+        if (tempObj == null) 
+            yield break;
+            
+        var otherID = tempObj.idObj;
+        Debug.Log(otherID == idObj ? "MATCH" : "NO MATCH");
         if (otherID == idObj)
         {
-            Debug.Log("MATCH");
+            matchEvent.Invoke();
+        }
+        else
+        {
+            noMatchEvent.Invoke();
+            yield return new WaitForSeconds(0.5f);
+            noMatchDelayedEvent.Invoke();
         }
     }
+
+
 }
